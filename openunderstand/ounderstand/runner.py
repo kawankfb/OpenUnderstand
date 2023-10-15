9448171f829d89be7649aaf6ef4e90d5931ebf76
KAVAN FARHADIBANEH <kawan.kfb@gmail.com>
2023-10-15 15:49:38 +0330

    first commit of development branch, direct copy of dev branch on 15 october 2023

--- openunderstand/ounderstand/runner.py
+++ openunderstand/ounderstand/runner.py
@@ -2,9 +2,7 @@ from multiprocessing import cpu_count, Pool
 from ounderstand.parsing_process import process_file, get_files
 
 
-def runner(path_project: str = ""):
+def runner(path_project: str = "D:\\PHD\\OpenUnderstand-dev\\OpenUnderstand-dev\\benchmark"):
     files = get_files(path_project)
-    with Pool(cpu_count()) as pool:
-        pool.map_async(process_file, files)
-        pool.close()
-        pool.join()
+    for item in files:
+        process_file(item)
